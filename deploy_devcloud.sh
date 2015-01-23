56c3ff064dad97f0f27ad3c42c7049dfd8ded768
vigneshvar <openstack@ubuntu-server-x8664.(none)>
2015-01-24 01:55:35 +0530

    deploy_devcloud.sh - changing repo to my branch

--- deploy_devcloud.sh
+++ deploy_devcloud.sh
@@ -14,8 +14,8 @@ export KEY_NAME=vignesh
 export env_file=~/devcloud_admin_cred.sh
 export ssh_user=ubuntu
 export git_protocol=https
-#export puppet_modules_source_repo=https://github.com/bodepd/puppet-rjil
-#export puppet_modules_source_branch=test_proxy
+export puppet_modules_source_repo=https://github.com/viggates/puppet-rjil
+export puppet_modules_source_branch=devcloud
 #export python_jiocloud_source_repo=git+https://github.com/bodepd/python-jiocloud
 #export python_jiocloud_source_branch=deprecate_etcd
 bash -x build_scripts/deploy.sh
